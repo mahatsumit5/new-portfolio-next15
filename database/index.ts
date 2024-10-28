0c2243d050b66cdcc52740dc05fe7db49427a3a0
mahatsumit5 <mahatsumit5@gmail.com>
2024-10-29 00:15:24 +1100

    ...

--- database/index.ts
+++ database/index.ts
@@ -3,6 +3,7 @@ const MONGO_URI = process.env.MONGODB_URI;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const cached = (global as any).mongoose || { conn: null, promise: null };
 export const connectToDatabase = async () => {
+  // console.log(MONGO_URI);
   if (!MONGO_URI) throw new Error("mongouri is missing");
   cached.promise =
     cached.promise ||
